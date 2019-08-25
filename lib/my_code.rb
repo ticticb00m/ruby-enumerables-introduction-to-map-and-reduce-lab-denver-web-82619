cbcc9070071d7d7c0094ba491955594a65fb93d2
mohamad farahmandifard <ticticb80om@gmail.com>
2019-08-25 17:53:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,7 +58,7 @@ def reduce_to_all_true(source_array)
  end
  
  def reduce_to_any_true(source_array)
-   if source_array.any == true 
+   if source_array.any? == true 
      return true
    else
      return false
