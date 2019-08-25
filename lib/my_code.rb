98bc18feb453aa829ca9d94e6a8ecf1a8b44e07d
mohamad farahmandifard <ticticb80om@gmail.com>
2019-08-25 17:01:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,5 +50,9 @@ def reduce_to_total(source_array, starting_point=0)
  end
    
 def reduce_to_all_true(source_array)
-  source_array.any?
+   if source_array.any? == true
+     return "true"
+   else 
+     return "false"
+   end
   
