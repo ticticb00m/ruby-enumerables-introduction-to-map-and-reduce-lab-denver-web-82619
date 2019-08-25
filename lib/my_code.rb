644e62eac7f4cd2f1ecf7832c69017f1f84f8cbd
mohamad farahmandifard <ticticb80om@gmail.com>
2019-08-25 15:19:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,6 +12,10 @@ end
 
 def map_to_no_change(source_array)
   answer = []
-  answer.push(source_array)
-  return
+  i = 0 
+  while i < source_array.length
+  answer.push(source_array[i])
+  i += 1 
+end
+return answer
 end
