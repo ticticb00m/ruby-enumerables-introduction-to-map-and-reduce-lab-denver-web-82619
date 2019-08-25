59897f4da3f28de4eeb2c28712cf11961c177366
mohamad farahmandifard <ticticb80om@gmail.com>
2019-08-25 15:13:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # My Code here....
+
+def map_to_negativize(source_array)
+  answer = []
+  i = 0 
+  while i < source_array.length
+    answer.push(source_array[i]*-1)
+  end
+  return answer
+end
